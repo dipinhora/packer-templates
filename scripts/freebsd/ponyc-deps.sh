c277fc54cfb5d7b8c89a792c1c5024d7dbb01d54
Dipin Hora <dipin@wallaroolabs.com>
2018-07-28 19:38:37 +0000

    Add freebsd 11.2 VM support and fix llvm 3.9 download issue hopefully

--- scripts/freebsd/ponyc-deps.sh
+++ scripts/freebsd/ponyc-deps.sh
@@ -5,7 +5,6 @@ set -x
 
 sudo pkg install -y rsync
 sudo pkg install -y gmake
-sudo pkg install -y llvm39
 sudo pkg install -y pcre2
 sudo pkg install -y libunwind
-
+sudo pkg add -y https://pkg.freebsd.org/FreeBSD:11:amd64/release_2/All/llvm39-3.9.1_8.txz
