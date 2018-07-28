6cf807ab6d972fc2517bf0a283abf7938b6a411c
Dipin Hora <dipin@wallaroolabs.com>
2018-07-28 20:16:11 +0000

    Add more memory and openssl dependency

--- scripts/freebsd/ponyc-deps.sh
+++ scripts/freebsd/ponyc-deps.sh
@@ -3,8 +3,12 @@
 set -e
 set -x
 
+cat /etc/pkg/FreeBSD.conf
+
 sudo pkg install -y rsync
 sudo pkg install -y gmake
 sudo pkg install -y pcre2
 sudo pkg install -y libunwind
+sudo pkg install -y libopenssl
+sudo pkg install -y libopenssl-devel
 sudo pkg add -y https://pkg.freebsd.org/FreeBSD:11:amd64/release_2/All/llvm39-3.9.1_8.txz
