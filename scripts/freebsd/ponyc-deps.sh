c18622c53b2c8445e51fd640ec9c4509bab2441d
Dipin Hora <dipin@wallaroolabs.com>
2018-07-28 21:24:26 +0000

    Switch openssl with libressl

--- scripts/freebsd/ponyc-deps.sh
+++ scripts/freebsd/ponyc-deps.sh
@@ -9,6 +9,6 @@ sudo pkg install -y rsync
 sudo pkg install -y gmake
 sudo pkg install -y pcre2
 sudo pkg install -y libunwind
-sudo pkg install -y libopenssl
-sudo pkg install -y libopenssl-devel
+sudo pkg install -y libressl
+sudo pkg install -y libressl-devel
 sudo pkg add -y https://pkg.freebsd.org/FreeBSD:11:amd64/release_2/All/llvm39-3.9.1_8.txz
