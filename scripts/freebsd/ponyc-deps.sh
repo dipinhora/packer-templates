874f54f7961fd0af6a277e7948bdf32758dc6cbd
Dipin Hora <dipin@wallaroolabs.com>
2018-07-28 22:20:46 +0000

    add llvm39 deps

--- scripts/freebsd/ponyc-deps.sh
+++ scripts/freebsd/ponyc-deps.sh
@@ -3,11 +3,13 @@
 set -e
 set -x
 
-cat /etc/pkg/FreeBSD.conf
-
 sudo pkg install -y rsync
 sudo pkg install -y gmake
 sudo pkg install -y pcre2
 sudo pkg install -y libunwind
 sudo pkg install -y libressl-devel
+sudo pkg install -y libedit
+sudo pkg install -y libxml2
+sudo pkg install -y python27
+sudo pkg install -y perl5
 sudo pkg add http://pkg.freebsd.org/FreeBSD:11:amd64/release_2/All/llvm39-3.9.1_8.txz
